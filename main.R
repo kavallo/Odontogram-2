ff05e59cedfe00629a7dd67ebf4f97028cc8d81e
DRBCodeBase <drblanchette@gmail.com>
2013-10-09 13:07:17 -0500

    Main application
    Establishes named vectors and calls the drawing routines as necessary. A vector of surface level information corresponding to a single exam is needed for one odontogram.

---
+++ main.R
@@ -0,0 +1,42 @@
+#Main Application for Odontogram Creation
+
+library(shape)
+
+source("draw.four.R")
+source("draw.five.R")
+source("draw.mouth")
+
+
+data <- read.table("file.csv", sep = ",", header = TRUE)
+data.sorted <- data[order(data$PID, data$Visit),]
+attach(data.sorted)
+
+#####
+#Initialize Named Vectors (similar to dictionaries in Python)
+#####
+
+sex.vector <- c("1" = "Male", "2" = "Female")
+
+color.vector <- c("D" = "black",
+                  "d" = "black",
+                  "M" = "black",
+                  "m" = "black",
+                  "F" = "black",
+                  "f" = "black",
+                  "S" = "white",
+                  "s" = "white",
+                  "U" = "light pink",
+                  "u" = "light pink",
+                  "P" = "green",
+                  "p" = "green",
+                  "Y" = "red",
+                  "y" = "red",
+                  "C" = "gold",
+                  "c" = "gold")
+
+#Output the odontograms to PDF for easy reviewing and printing. 
+pdf("odontograms.pdf", height = 4)
+  for (i in seq(1,1867)){
+    draw.mouth(data.sorted[i,])
+  }
+dev.off()
